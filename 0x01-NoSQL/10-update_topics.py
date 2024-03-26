575ea4a7cce1e606696ec9c2347fb581c9aacd67
Atala2000 <atalakidi@gmail.com>
2024-03-26 23:55:24 +0300

    10

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -2,5 +2,10 @@
 """
 Module to update topics
 """
+
+
 def update_topics(mongo_collection: object, name: str, topics: list):
+    """
+    Changes many documents
+    """
     mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
