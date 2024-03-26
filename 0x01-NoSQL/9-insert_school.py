c80af3639bed7e9b6a4982d32f04829903fc7207
Atala2000 <atalakidi@gmail.com>
2024-03-26 21:55:59 +0300

    9

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -2,6 +2,13 @@
 """
 Module to insert document
 """
-def insert_school(mongo_collection, **kwargs):
+def insert_school(mongo_collection: object, **kwargs) -> int:
+        """"
+        Function that inserts document with arguments
+        Args:
+            mongo_collection (object): Object containing query
+        Returns:
+            Inserted id
+        """
         object_id =  mongo_collection.insert_one(kwargs)
         return object_id.inserted_id
